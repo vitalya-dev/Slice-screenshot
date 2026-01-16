d4f3c340b95bc7c0b9c1b722c8f4454f13d14d67
Vitalya <vitalya.dev@gmail.com>
2026-01-17 00:36:41 +0300

    Command Implementation and Testing.

--- slice_screenshot.sh
+++ slice_screenshot.sh
@@ -10,18 +10,25 @@ fi
 INPUT_FILE="$1"
 
 # 2. Get the width of the image automatically
-# We use 'identify -format "%w"' to extract just the width number
 WIDTH=$(magick identify -format "%w" "$INPUT_FILE")
 
-echo "Input File: $INPUT_FILE"
-echo "Detected Width: $WIDTH pixels"
-
 # 3. Calculate the height for A4 format
-# A4 Aspect Ratio is approx 1.4142 (297mm / 210mm)
+# A4 Aspect Ratio is approx 1.4142
 RATIO=1.4142
-
-# We use awk to multiply width by ratio and round to nearest integer
 HEIGHT=$(awk -v w="$WIDTH" -v r="$RATIO" 'BEGIN { printf "%.0f", w * r }')
 
-echo "Calculated Slice Height: $HEIGHT pixels"
-echo "Slice gv ${WIDTH}x${HEIGHT}"
+echo "Processing: $INPUT_FILE"
+echo "Slicing with dimensions: ${WIDTH}x${HEIGHT}"
+
+# 4. Execute the Magick command
+# We use "${INPUT_FILE%.*}" to get the filename without the extension
+OUTPUT_NAME="${INPUT_FILE%.*}_part_%d.jpg"
+
+magick "$INPUT_FILE" \
+    -crop "${WIDTH}x${HEIGHT}" \
+    +repage \
+    -extent "${WIDTH}x${HEIGHT}" \
+    -reverse \
+    "$OUTPUT_NAME"
+
+echo "Done! created slices starting with ${OUTPUT_NAME}"
